956f881bd4db0ee0af74d3a6c01c06ed48a70b41
MaxDev6966 <85440084+MaxGamingOfficalYT@users.noreply.github.com>
2022-11-26 17:28:54 +0000

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# Status-Role-Bot
-Join https://dsc.gg/maxgaming-yt for more support!
+# Status_Role
+
+## [**DISCORD SUPPORT SERVER INVITE**](https://dsc.gg/milanio.dev)
+> 💪 **Host on Repit:** [**Repl.it Fork**](https://replit.com/@MaxGamingOfficalYT/Status-Role-Bot?v=1)
+
+## Installation | How to use the Handler
+
+ **1.** Install [node.js v16+](https://nodejs.org/) or higher
+
+ **2.** Download this repo and unzip it    |    or git clone it
+
+ **3.** Install all of the packages with **`npm install`**     |  the important packages are   **`npm install discord.js enmap`**
+
+ **4** Fill in the parameters, RIGHT in `config.json`!
+
+ **5.** start the bot with **`node bot.js`**!
+  
+***
+
+## [Discord Server 😎](https://dsc.gg/maxgaming.yt)
+
+***
+
+## SUPPORT ME AND MAX GAMING YT
+
+> You can always Support me by inviting one of my **own Discord Bots**
+
+[MAX GAMING YT](https://dsc.gg/maxgaming.yt) | [Jerry Music](https://dsc.gg/jerry.milanio) | [Sound Wave](https://dsc.gg/sound.milanio)
+
+# Credits
+
+> If consider using this Code, make sure to credit me!
---
+++ module.js
@@ -0,0 +1,63 @@
+// Commands Module
+const config = require('../../botconfig/config.json');
+const {
+  MessageEmbed
+} = require('discord.js');
+
+module.exports = {
+  name: '',
+  aliases: [],
+  usage: '',
+  description: '',
+  cooldown: 0,
+  userPermissions: [],
+  botPermissions: [],
+  ownerOnly: false,
+  toggleOff: false,
+
+  run: async (client, message, args, ee) => {
+    try {
+      // Start Coding
+    } catch (err) {
+      console.log(err)
+    }
+  }
+}
+// Commands Module End
+
+// ---------------------------------------------------
+
+// Slash Commands Module
+const config = require('../../botconfig/config.json');
+const {
+  MessageEmbed
+} = require('discord.js');
+
+module.exports = {
+  name: '',
+  description: '',
+  cooldown: 0,
+  userPermissions: [],
+  botPermissions: [],
+  ownerOnly: true,
+  toggleOff: true,
+
+  run: async (client, interaction, args, ee) => {
+    try {
+      // Start Coding
+    } catch (err) {
+      console.log(err)
+    }
+  }
+}
+// Slash Commands Module End
+
+/**
+ * @INFO
+ * Bot Coded by MAX GAMING YTᵈᵉᵛ#8643| https://discord.gg/u9x5gdZ6Vw
+ * @INFO
+ * Work for MAX GAMING YT | https://discord.gg/u9x5gdZ6Vw
+ * @INFO
+ * Please Mention Us MAX GAMING YT, When Using This Code!
+ * @INFO
+ */
